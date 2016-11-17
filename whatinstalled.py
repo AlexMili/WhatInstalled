09d49f2cc555b2f50bd5db4584ed97a6df878cca
Max <AlexMili@users.noreply.github.com>
2016-11-17 11:29:00 +0100

    Add aptitude and apt + bower

--- whatinstalled.py
+++ whatinstalled.py
@@ -3,7 +3,7 @@ import os
 history_file = os.path.expanduser("~")+"/.bash_history"
 f = open(history_file,"r")
 
-keywords = ["pip", "tar", "brew", "apt-get", "install", "luarocks", "easy_install", "gem", "npm"]
+keywords = ["pip", "tar", "brew", "apt-get", "aptitude", "apt", "install", "luarocks", "easy_install", "gem", "npm", "bower"]
 
 for line in f:
 	for item in keywords:
