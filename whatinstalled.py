96588f427d289d7d40ae1cd04cb2fc556ea2f2cc
AlexMili <mili.alexandre@gmail.com>
2016-11-23 15:36:58 +0100

    re-organize keywords + argparse

--- whatinstalled.py
+++ whatinstalled.py
@@ -1,12 +1,59 @@
 import os
+import argparse
 
-history_file = os.path.expanduser("~")+"/.bash_history"
-f = open(history_file,"r")
+keywords = {\
+	"mac":[\
+		"brew install",\
+		"brew cask install",\
+		"port install"\
+	],\
+	"linux":[\
+		"apt-get install",\
+		"aptitude install",\
+		"yum install",\
+		"pacman install",\
+		"dpkg -i",\
+		"dnf install",\
+		"zypper in",\
+		"make install",\
+		"tar "\
+	],\
+	"lua":[\
+		"luarocks install",\
+		"luarocks make"\
+	],\
+	"python":[\
+		"pip install",\
+		"easy_install",\
+		"conda install"\
+	],\
+	"ruby":[\
+		"gem install",\
+		"rvm install"\
+	],
+	"node":[\
+		"npm install",\
+		"bower install"\
+	],\
+}
 
-keywords = ["pip", "tar", "brew", "apt-get", "aptitude", "apt", "install", "luarocks", "easy_install", "gem", "npm", "bower"]
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Retrieve what you installed on your system using bash history')
+	parser.add_argument('-f', '--file',		dest='bash_file', 	type=str, help="custom file to parse", 		default="~/.bash_history")
+	parser.add_argument('-p', '--profile',	dest='profile', 	type=str, help="specific profile to use",	default=None)
+	args = parser.parse_args()
 
-for line in f:
-	for item in keywords:
-		if item in line:
-			print(line[:-1])
-			break
+	history_file = os.path.expanduser(args.bash_file)
+	f = open(history_file,"r")
+
+	if(args.profile != None and args.profile in keywords):
+		keywords = {args.profile:keywords[args.profile]}
+	elif(args.profile != None and args.profile not in keywords):
+		print("\n[ERROR]Profile doesn't exist\n")
+		exit(0)
+
+	for line in f:
+		for category in keywords:
+			for item in keywords[category]:
+				if item in line:
+					print("["+category+"//"+item+"]"+str(line[:-1]))
